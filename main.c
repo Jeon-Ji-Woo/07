85d0664653d351052427718e2530dc5d6d1329a4
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2023-10-19 16:18:49 +0900

    실습5

--- main.c
+++ main.c
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int counter;
+void sub(void);
 
-	void set_counter(void)
-{
-	counter = 20;
-}
 int main(void)
 {
-	printf("counter=%d\n",counter);
-	set_counter();
-	printf("counter=%d\n",counter);
+	int i;
+	for(i=0;i<3;i++)
+	{
+		sub();
+	}
 	
 	return 0;
+}
+void sub(void)
+{
+	int auto_count = 0;
+	static int static_count = 0;
+	auto_count++;
+	static_count++;
+	printf("auto_count=%d\n",auto_count);
+	printf("static_count=%d\n",static_count);
 }
